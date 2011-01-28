cda087c10e2ac48d67bed1f31974481f0e953b42
Peter Foster <=>
2011-01-28 18:42:34 -0500

    Version 3.2

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -3,7 +3,7 @@ class PublicationsController < ApplicationController
   # GET /publications.xml
   def index
     @years = Publication.find(:all).map {|p| p.year}.uniq
- 	 
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => nil }
