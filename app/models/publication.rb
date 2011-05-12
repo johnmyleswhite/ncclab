196483f759cab05169003e36ae60264d9f3968fd
Peter Foster <=>
2011-05-12 12:44:53 -0400

    Update 5/12/11

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -6,5 +6,24 @@ class Publication < ActiveRecord::Base
       return self.year
     end
   end
+  
+  def year_sortable
+   if self.year.nil?
+    return 3000
+   else 
+    return self.year
+   end
+  end
+  
+  def self.sorted_by_years_and_names
+  	sorted = Publication.find(:all).sort do |a, b|
+  		if a.year_sortable > b.year_sortable
+  			-1
+  		elsif a.year_sortable < b.year_sortable
+  			1
+  		else
+  			a.authors <=> b.authors
+  		end
+  	end
+  end
 end
-
